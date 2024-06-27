e2de6397e8a14266a6600c1f8fedabb687179865
verdeverdad <sofia.16d@gmail.com>
2024-06-27 02:09:02 -0300

    primer commit

---
+++ costa-de-oro/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './component/navbar';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        
+        <Navbar></Navbar>
+      </div>
+    </>
+  )
+
+}
+
+
+export default App;
